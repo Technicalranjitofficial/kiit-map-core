2a53534d769a0f7e1c821dfb3b4303aaa65e5710
Ranjit Das <dranjitkumar16@gmail.com>
2026-06-21 21:53:41 +0530

    Force map packages install and fix import path

--- nextjs-app/components/CampusMap.jsx
+++ nextjs-app/components/CampusMap.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState, useRef } from 'react';
-import Map, { Marker } from 'react-map-gl';
+import Map, { Marker } from 'react-map-gl/maplibre';
 import maplibregl from 'maplibre-gl';
 import { io } from 'socket.io-client';
 
